96f142162324458d8a71e21b4a1139bd7f82cd60
Kautilya Vaidya <kvaidya221@gmail.com>
2021-06-24 12:55:44 +0530

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OneFPS.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions opt) : base(opt)
+        {
+
+        }
+    }
+}
