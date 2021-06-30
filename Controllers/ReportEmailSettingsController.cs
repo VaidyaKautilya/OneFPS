5c29d309f5bdd5944a15c2bf0d3fa9fbfff3fdc4
Kautilya Vaidya <kvaidya221@gmail.com>
2021-06-30 11:49:25 +0530

    Commit

---
+++ Controllers/ReportEmailSettingsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneFPS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportEmailSettingsController : ControllerBase
+    {
+    }
+}
